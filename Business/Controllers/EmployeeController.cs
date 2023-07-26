1de8458d07fe03832937f35a2f2b953183daa195
Thinh <nxthinh.19it5@vku.udn.vn>
2023-07-27 01:53:19 +0700

    first commit

---
+++ Business/Controllers/EmployeeController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Business.Models;
+
+namespace Business.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly string _connectionString;
+
+        // Constructor của EmployeeController
+        public EmployeeController()
+        {
+            _connectionString = "Server=DESKTOP-RJGQ1SN;Database=Employee;Trusted_Connection=True;";
+        }
+
+        // Phương thức hiển thị danh sách nhân viên
+        public IActionResult Index()
+        {
+            List<EmployeeModel> employeeList = new List<EmployeeModel>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Id, FirstName, LastName, Email FROM Staff";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int employeeID = reader.GetInt32(0);
+                            string firstName = reader.GetString(1);
+                            string lastName = reader.GetString(2);
+                            string email = reader.GetString(3);
+
+                            var employee = new EmployeeModel
+                            {
+                                Id = employeeID,
+                                FirstName = firstName,
+                                LastName = lastName,
+                                Email = email
+                            };
+
+                            employeeList.Add(employee);
+                        }
+                    }
+                }
+            }
+
+            // Truyền danh sách nhân viên vào View thông qua Model
+            return View(employeeList);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeModel Staff)
+        {
+            if (ModelState.IsValid)
+            {
+                using (SqlConnection connection = new SqlConnection("Server=DESKTOP-RJGQ1SN;Database=Employee;Trusted_Connection=True;"))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO Staff (FirstName, LastName, Email) OUTPUT INSERTED.Id VALUES (@FirstName, @LastName, @Email);";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@FirstName", Staff.FirstName);
+                        command.Parameters.AddWithValue("@LastName", Staff.LastName);
+                        command.Parameters.AddWithValue("@Email", Staff.Email);
+
+                        int newId = (int)command.ExecuteScalar();
+                        // Dùng giá trị 'newId' trong code của bạn nếu cần thiết.
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(Staff);
+        }
+    }
+}
---
+++ Business/wwwroot/js/userIndex.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    $('#usersTable').DataTable({
+        dom:
+            "<'row'<'col-sm-3'l><'col-sm-6 text-center'B><'col-sm-3'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: [
+        ],
+        language: {
+            "sDecimal": ",",
+            "sEmptyTable": "No data available in the table",
+            "sInfo": "_TOTAL_ from the recording _START_ - _END_ Showing records between",
+            "sInfoEmpty": "No record",
+            "sInfoFiltered": "(_MAX_ found in the registry)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "On Page _MENU_ Show Record",
+            "sLoadingRecords": "Loading...",
+            "sProcessing": "Processing...",
+            "sSearch": "Search:",
+            "sZeroRecords": "No matching records found",
+            "oPaginate": {
+                "sFirst": "First",
+                "sLast": "End",
+                "sNext": "Next",
+                "sPrevious": "Back"
+            },
+            "oAria": {
+                "sSortAscending": ": enable ascending column sort",
+                "sSortDescending": ": enable descending column sort"
+            },
+            "select": {
+                "rows": {
+                    "_": "%d record selected",
+                    "0": "",
+                    "1": "1 record selected"
+                }
+            }
+        }
+    });
+});
